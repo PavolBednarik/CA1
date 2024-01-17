b32977b001fa6d4b4f0eb54f077d2a4b54d53b69
PavolBednarik <152331129+PavolBednarik@users.noreply.github.com>
2024-01-17 19:57:27 +0000

    creating fileReader

--- src/ca1/CA1.java
+++ src/ca1/CA1.java
@@ -4,6 +4,9 @@
  */
 package ca1;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+
 /**
  *
  * @author pavol
@@ -13,8 +16,29 @@ public class CA1 {
     /**
      * @param args the command line arguments
      */
+    
+    public static void fileReading (){
+        try {
+            BufferedReader br = new BufferedReader(new FileReader("students.txt"));
+            String firstLine;
+            while ((firstLine = br.readLine()) !=null){
+            String[] nameSurname = firstLine.split(" ");
+            if (nameSurname.length == 2){
+                String name = nameSurname[0];
+                String surname = nameSurname[1];
+                int numClasses = Integer.parseInt(br.readLine());
+                String studNumber = br.readLine();
+                Student student = new Student (name,surname,numClasses,studNumber);
+            }else {
+            }    
+            }
+        } catch (Exception e){
+            System.out.println("invalid data");
+        }
+    }
+    
     public static void main(String[] args) {
-        // TODO code application logic here
+       fileReading ();
     }
     
 }
